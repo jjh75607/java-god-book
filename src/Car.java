620eb388fd969bd36eadba45af607116e52be4ae
jjh75607 <dkrak5738@naver.com>
2019-05-19 15:28:02 +0900

    third chapter finish

---
+++ src/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+
+    int speed;
+    int distance;
+
+    String color;
+
+    Car() {}
+
+    public void speedUp() {
+        speed = speed + 5;
+    }
+
+    public void breakDown() {
+        speed = speed - 10;
+    }
+
+    public int getCurrentSpeed() {
+        return speed;
+    }
+
+}
