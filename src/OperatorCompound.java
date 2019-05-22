934fc78600af22300781f093482004291387e3bd
jjh75607 <dkrak5738@naver.com>
2019-05-22 23:32:16 +0900

    fifth chapter continue

---
+++ src/OperatorCompound.java
@@ -0,0 +1,28 @@
+public class OperatorCompound {
+
+    public static void main(String[] args) {
+
+        OperatorCompound compound = new OperatorCompound();
+        compound.compound();
+
+    }
+
+    public void compound() {
+        int intValue = 10;
+
+        intValue += 5;
+        System.out.println(intValue);
+
+        intValue -= 5;
+        System.out.println(intValue);
+
+        intValue *= 5;
+        System.out.println(intValue);
+
+        intValue /= 5;
+        System.out.println(intValue);
+
+        intValue %= 5;
+        System.out.println(intValue);
+    }
+}
---
+++ src/OperatorUnary.java
@@ -0,0 +1,18 @@
+public class OperatorUnary {
+
+    public static void main(String[] args) {
+
+        OperatorUnary operator = new OperatorUnary();
+        operator.unary();
+
+    }
+
+    public void unary() {
+        int intValue = -10;
+        int result = +intValue;
+        System.out.println(result);
+
+        result = -10;
+        System.out.println(result);
+    }
+}
