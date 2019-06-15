a86a365d4dc6981d97fbe951177455f35a4105d6
jjh75607 <dkrak5738@naver.com>
2019-06-15 14:58:39 +0900

    if finish

---
+++ src/ControlIfAndOr.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class ControlIfAndOr {
+
+	public static void main(String[] args) {
+		ControlIfAndOr control = new ControlIfAndOr();
+		control.ifAndOr();
+
+	}
+
+	public void ifAndOr() {
+		int age = 25;
+		boolean isMarried = true;
+
+		if (age > 25 && isMarried) {
+			System.out.println("Age is over 25 and Married");
+		}
+		if (age > 25 || isMarried) {
+			System.out.println("Age is over 25 or Married");
+		}
+	}
+
+	public void tripleOrAnd() {
+		int age = 25;
+		boolean isMarried = true;
+		double height = 180;
+
+		if ((age > 25 || isMarried) && height >= 180) {
+			System.out.println("Age is over 25 and Married");
+		}
+	}
+
+
+
+}
