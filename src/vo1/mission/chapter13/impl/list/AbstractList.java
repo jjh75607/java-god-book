0963cefca849fcb2d8ba15bcf4fcf63f00283b1d
jjh75607 <dkrak5738@naver.com>
2019-11-17 17:18:49 +0900

    chapter13 mission

---
+++ src/vo1/mission/chapter13/impl/list/AbstractList.java
@@ -0,0 +1,21 @@
+package vo1.mission.chapter13.impl.list;
+
+public abstract class AbstractList implements List {
+
+    public abstract void clear();
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void update(int index, Object value) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+}
---
+++ src/vo1/mission/chapter13/impl/list/List.java
@@ -0,0 +1,8 @@
+package vo1.mission.chapter13.impl.list;
+
+public interface List {
+
+    public void add();
+    public void update(int index, Object value);
+    public void remove(int index);
+}
